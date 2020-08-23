12abc20b4cdab464621273253c0c29b36912c66f
Unnati Verma <unavee.24@gmail.com>
2020-08-24 00:48:56 +0530

    Updated AppBar

--- src/components/AppBarComponent.jsx
+++ src/components/AppBarComponent.jsx
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,7 +24,6 @@ function AppBarComponent() {
           <Typography variant="h6" className={classes.title}>
             GITHUB FORKS
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
